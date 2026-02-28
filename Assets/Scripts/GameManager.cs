c8ac894e411b9d3c748262bfaa0062f160dc4118
JRissberger <JRissberger@gmail.com>
2026-02-27 23:35:13 -0500

    chud inventory that might work, testing later

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -58,46 +58,73 @@ public class GameManager : MonoBehaviour
     //Called when an inventory button is pressed
     //Either updates the selected item if null, or moves the selected item to the new spot
     //i refers to the slot of the array to check
-    public void MoveItem(GameObject button)
+    //First pass of this code is gonna be a mess im so sorry, inventories suck
+    //There is absolutely a better way to indicate spots I'm just tired and can't think of it rn
+    public void MoveItem(int i)
     {
+
         //No selected item, store the one in the corresponding array slot
         if (selectedItem == null)
         {
             ////TODO: highlight button?
-            //RetrieveItem(i);
-            //selectedItemIndex = i;
-            //return;
-
-            //Get the child item of the button that called this
-            selectedItem = button.GetComponentInChildren<Item>();
-            Debug.Log(selectedItem);
+            RetrieveItem(i);
+            selectedItemIndex = i;
+            return;
         }
 
         //Item already being moved, update existing info
-        //Move item in selected spot if needed
+        Item previousItem = EquipItem(i);
 
-        
+        //If there's an item passed back, set it to the first item's spot
+        if (previousItem != null)
+        {
+            EquipItem(selectedItemIndex);
+        }
 
         ////Null out selected item and index
-        //selectedItem = null;
-        //selectedItemIndex = -1;
+        selectedItem = null;
+        selectedItemIndex = -1;
+
+        //TODO: update text of buttons -- array of buttons to reference?
+        //In that case, may not need to pass in int and can just set listener on startup,,,,getting everything working first though
 
     }
 
     //Helper for MoveItem, gets an item from an array based on button index
-    //public void RetrieveItem(int i)
-    //{
-    //    //If index is >1, Eurydice array
-    //    if (i > 1)
-    //    {
-    //        i -= 2;
-    //        selectedItem = Eurydice.EquippedItems[i];
-    //    }
+    private void RetrieveItem(int i)
+    {
+        //If index is >1, Eurydice array
+        if (i > 1)
+        {
+            i -= 2;
+            selectedItem = Eurydice.EquippedItems[i];
+        }
 
-    //    //Orpheus array
-    //    else
-    //    {
-    //        selectedItem = Orpheus.EquippedItems[i];
-    //    }
-    //}
+        //Orpheus array
+        else
+        {
+            selectedItem = Orpheus.EquippedItems[i];
+        }
+    }
+
+    //Helper for MoveItem, runs equipItem on determined spot
+    private Item EquipItem(int i)
+    {
+        Item previousItem = null;
+        //If index is >1, Eurydice array
+        if (i > 1)
+        {
+            i -= 2;
+            previousItem = Eurydice.EquipItem(i, selectedItem);
+        }
+
+        //Orpheus array
+        else
+        {
+            previousItem = Orpheus.EquipItem(i, selectedItem);
+        }
+
+        //fallback, shouldn't get hit
+        return previousItem;
+    }
 }
