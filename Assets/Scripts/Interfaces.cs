07078bc93f0b2abd1c292daa8828e5a71124d4aa
Sgedelta <Sgeaston04@gmail.com>
2026-03-01 18:52:37 -0500

    moved interfaces to their own file

---
+++ Assets/Scripts/Interfaces.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public interface ICanEquip
+{
+    public Item[] EquippedItems { get; set; }
+
+    /// <summary>
+    /// Attempts to put the item in the slot of the inventory in the current index, and adjusts all internal flags as appropriate
+    /// </summary>
+    /// <param name="i">The index to put the item into, 0 based</param>
+    /// <returns>The item that was in that index previously, or null</returns>
+    public Item EquipItem(int i, Item item);
+
+    /// <summary>
+    /// Unequips the item listed, no matter the index. Returns true if the item was removed, false otherwise
+    /// </summary>
+    /// <param name="item">The item to remove</param>
+    /// <returns>true if the item was removed, false otherwise</returns>
+    public bool UnequipItem(Item item);
+
+    /// <summary>
+    /// Checks if the given item is currently equipped
+    /// </summary>
+    /// <param name="item">the item to check</param>
+    /// <returns>true if the item is equipped, false otherwise</returns>
+    public bool IsEquipped(Item item);
+
+}
+
+public interface ICanAttack
+{
+    /// <summary>
+    /// Scalar value applied to the base speed for the attack slider
+    /// </summary>
+    public float AttackSpeed { get; set; }
+
+    /// <summary>
+    /// Scalar value applied to the base acceptable hit range for attack slider
+    /// </summary>
+    public float AttackMissAllowance { get; set; }
+
+    /// <summary>
+    /// Scalar value applied to the base acceptable perfect hit range for attack slider
+    /// </summary>
+    public float AttackPerfectAllowance { get; set; }
+
+    /// <summary>
+    /// Amount of Sympathy gained by an attack
+    /// </summary>
+    public float AttackDamage { get; set; }
+
+    /// <summary>
+    /// An attack method, which takes an effectiveness (how accurately you hit the notes)
+    /// </summary>
+    /// <param name="effectiveness"></param>
+    public void Attack(float effectiveness);
+
+    /// <summary>
+    /// A event that is called on attack. Takes the effectiveness passed into attack. 
+    /// Should be run during each character's attack method for any rider effects -> need for Eurydice
+    /// </summary>
+    public System.Action<float> OnAttack { get; set; }
+}
+
+
+
+
