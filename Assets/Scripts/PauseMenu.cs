657435587dbd1fdde5473b43e647aef0cd92f411
matthewfong3 <mxf9125@rit.edu>
2026-03-25 01:30:39 -0400

    pause menu reworked and changed the default color of input to white

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -4,33 +4,14 @@ using UnityEngine.InputSystem;
 
 public class PauseMenu : MonoBehaviour
 {
-    public static PauseMenu instance;
-
-    [SerializeField] private GameObject _pauseMenu;
+    [SerializeField] private GameObject _pauseMenuPanel;
     private bool isGamePaused = false;
     //public bool IsEnabled { get; set; } = true;
 
-    private void Awake()
-    {
-        // If an instance already exists and it's not this, destroy this one
-        if (instance != null && instance != this)
-        {
-            Destroy(_pauseMenuCanvasGroup.gameObject);
-            return;
-        }
-
-        // Otherwise, set this as the instance
-        instance = this;
-
-        // Keep the object alive when loading new scenes
-        DontDestroyOnLoad(_pauseMenuCanvasGroup.gameObject);
-        _pauseMenuCanvasGroup.gameObject.SetActive(false);
-    }
-
     void Start()
     {
-        _pauseMenuCanvasGroup.blocksRaycasts = false;
-        _pauseMenuCanvasGroup.gameObject.SetActive(false);
+        _pauseMenuPanel = this.gameObject.transform.Find("PauseMenuPanel").gameObject;
+        _pauseMenuPanel.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -52,8 +33,7 @@ public class PauseMenu : MonoBehaviour
     {
         //if (IsEnabled)
         //{
-            _pauseMenuCanvasGroup.gameObject.SetActive(false);
-            _pauseMenuCanvasGroup.blocksRaycasts = false;
+            _pauseMenuPanel.gameObject.SetActive(false);
             Time.timeScale = 1f; // Resume normal game time
             isGamePaused = false;
         //}
@@ -63,8 +43,7 @@ public class PauseMenu : MonoBehaviour
     {
         //if (IsEnabled)
         //{
-            _pauseMenuCanvasGroup.gameObject.SetActive(true);
-            _pauseMenuCanvasGroup.blocksRaycasts = true;
+            _pauseMenuPanel.gameObject.SetActive(true);
             Time.timeScale = 0f; // Stop all time-based operations (movement, physics, yap)
             isGamePaused = true;
         //}
