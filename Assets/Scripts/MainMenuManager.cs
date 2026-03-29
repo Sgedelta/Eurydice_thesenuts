d08d50b812c33c774d748908bc82a8e0bf6d8b43
JRissberger <JRissberger@gmail.com>
2026-03-29 12:23:42 -0400

    Intro and controls

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -4,6 +4,9 @@ using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private string _firstLevel;
+    [SerializeField] private GameObject MainView;
+    [SerializeField] private GameObject ControlView;
+    [SerializeField] private GameObject IntroView;
 
     void Start()
     {
@@ -11,11 +14,26 @@ public class MainMenuManager : MonoBehaviour
 
     public void StartGame()
     {
+        //Resetting these in case it'd cause issues later
+        IntroView.SetActive(false);
+        MainView.SetActive(true);
         SceneManager.LoadScene(_firstLevel);
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void ToggleIntro()
+    {
+        MainView.SetActive(!MainView.activeSelf);
+        IntroView.SetActive(!IntroView.activeSelf);
+    }
+
+    public void ToggleControls()
+    {
+        MainView.SetActive(!MainView.activeSelf);
+        ControlView.SetActive(!ControlView.activeSelf);
     }
 }
