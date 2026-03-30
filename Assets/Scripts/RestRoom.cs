b72fbfc7dad32c77801a2179293fe4e07ee3bab9
Sgedelta <Sgeaston04@gmail.com>
2026-03-30 18:25:47 -0400

    some bugfix

--- Assets/Scripts/RestRoom.cs
+++ Assets/Scripts/RestRoom.cs
@@ -2,17 +2,7 @@ using UnityEngine;
 
 public class RestRoom : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        this.gameObject.SetActive(false);
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     
     public void OnRestore()
     {
